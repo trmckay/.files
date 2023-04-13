3535c0e21f06c0c5316ace54039c437273c57fb4
Trevor McKay <mail@trmckay.com>
2023-04-12 17:51:40 -0700

    clean up zsh prompt

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -1,47 +1,50 @@
-autoload -Uz vcs_info
-
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-
 export ZSH_LAST_EXIT=0
+function _component_exit_code {
+    echo "%(?..[%F{magenta}$ZSH_LAST_EXIT%f] )"
+}
+
+function _component_ssh {
+    if [[ ! -z $SSH_CLIENT ]]; then
+        echo "[%F{green}ssh: $(echo $SSH_CLIENT | cut -d' ' -f 1)%f] "
+    fi
+}
+
+function _component_machine {
+    function _pwd {
+        if [[ "$PWD" == "$HOME" ]]; then
+            echo "~"
+        else
+            echo "${PWD#"$HOME"/}"
+        fi
+    }
+    echo "[%F{cyan}%n@%m%f:%F{blue}$(_pwd)%f] "
+}
+
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' formats '[%F{red}%b%f] '
+zstyle ':vcs_info:*' actionformats '[%F{red}%b %a%f] '
+function _component_vcs {
+    echo "$vcs_info_msg_0_"
+}
+
+function _component_symbol {
+    echo "\n%# "
+}
 
 precmd() {
     export ZSH_LAST_EXIT=$?
     echo
     vcs_info
-    if [[ "$(pwd)" == "$HOME" ]]; then
-        print -Pn "\e]0;zsh - $(whoami)@$(_hostname)\a"
-    else
-        print -Pn "\e]0;zsh - $(whoami)@$(_hostname):$(basename "$(pwd)")\a"
-    fi
 }
 
-zstyle ':vcs_info:*' formats '[%F{red}%b%f] '
-zstyle ':vcs_info:*' actionformats '[%F{red}%b %a%f] '
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 set -o promptsubst
 
-NL=$'\n'
-
-function _hostname() {
-    hostname -s 2>/dev/null || hostnamectl hostname 2>/dev/null || echo "localhost"
-}
-
-function _pwd {
-    if [[ "$PWD" == "$HOME" ]]; then
-        echo "~"
-    else
-        echo "${PWD#"$HOME"/}"
-    fi
-}
-
-function _exit_code {
-    echo "%(?..[%F{magenta}$ZSH_LAST_EXIT%f] )"
-}
-
-function _ssh {
-    if [[ ! -z $SSH_CLIENT ]]; then
-        echo "[%F{green}ssh: $(echo $SSH_CLIENT | cut -d' ' -f 1)%f]"
-    fi
-}
-
-prompt='[%F{cyan}%n@%m%f:%F{blue}$(_pwd)%f] $vcs_info_msg_0_$(_ssh)$(_exit_code)${NL}%# '
+prompt='''\
+$(_component_machine)\
+$(_component_ssh)\
+$(_component_vcs)\
+$(_component_exit_code)\
+$(_component_symbol)\
+'''
